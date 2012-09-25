2bb8a0db2ee1029d3d10238a9abf5c92d8385c99
yukinoraru <yukinoraru@gmail.com>
2012-09-25 20:34:54 +0900

    Implement 'geojp' command and some fixes

--- lib/geocoder_jp.rb
+++ lib/geocoder_jp.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require 'geocoder_jp/version'
+require_relative 'geocoder_jp/version'
 require 'active_support/core_ext'
 require 'uri'
 require 'pp'
@@ -7,12 +7,16 @@ require 'net/http'
 require 'kconv'
 
 module GeocoderJP
-  RequestError = Class.new(StandardError)
-  NotFound     = Class.new(StandardError)
+  RequestError    = Class.new(StandardError)
+  NotFound        = Class.new(StandardError)
+  TooManyRequests = Class.new(StandardError)
 
   def self.get(address)
-    raise ArgumentError if address.blank?
-    uri = URI.parse(URI.encode("http://www.geocoding.jp/api/?v=1.1&q=#{address}"))
+    address = address.toutf8 unless address.nil?
+    raise ArgumentError, "Please input address." if address.blank?
+    url = "http://www.geocoding.jp/api/?v=1.1&q=#{address}".toutf8
+
+    uri = URI.parse(URI.encode(url))
 
     # GETリクエスト送出
     #Net::HTTP.get_print(uri); exit
@@ -26,14 +30,18 @@ module GeocoderJP
     xml    = Hash.from_xml(response.body)
     result = xml["result"]
 
-    # 存在しない場合は004が返却
-    raise NotFound if result["error"] == "004"
-
-    # それ以外はエラーコードと共にRequestErrorを返却
-    raise RequestError, "Request failed. Error code=#{result['error']}" if result.include?("error")
+    # エラーが存在する場合は例外を発生
+    case result["error"]
+    when "003"
+      raise TooManyRequests, "DO NOT request one or more times in 5 seconds."
+    when "004"
+      raise NotFound
+    else
+      # それ以外はエラーコードと共にRequestErrorを返却
+      raise RequestError, "Request failed. Error code=#{result['error']}" if result.include?("error")
+    end
 
     return result
-
   end
 
   def self.escape_xml(input)
--- spec/geocoder_jp_spec.rb
+++ spec/geocoder_jp_spec.rb
@@ -32,6 +32,21 @@ describe "GeocoderJP module" do
       response = GeocoderJP.get("東京タワー")
       response["coordinate"]["lat"].should_not be_empty
     end
+
+    it "'東京タワー'を様々な文字コードで入力する(時間かかります)" do
+      [
+        Kconv::JIS,
+        Kconv::EUC,
+        Kconv::SJIS,
+        Kconv::UTF8,
+      ].each do |encoding|
+        spot_name = Kconv.kconv("東京タワー", encoding)
+        response = GeocoderJP.get(spot_name)
+        response["coordinate"]["lat"].should_not be_empty
+        sleep 5
+      end
+    end
+
   end
 
 end
